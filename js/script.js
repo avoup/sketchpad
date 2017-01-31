37a30d420609453d95d072c270e5494d70cccf9d
lasha <avoup.g@gmail.com>
2017-01-31 18:44:15 +0400

    step 2

--- js/script.js
+++ js/script.js
@@ -1,20 +1,71 @@
+var size = 25;
+var color = "white";
+
+
 $(document).ready(function(){
-	var i = 0;
-  var size = prompt('enter number of squares');
-  var mult = size * size;
-  var squareSize = 500/size;
 
-  for(i = 0; i < mult; i++){
-		$('#wrap').append('<div class="block"></div>');
 
+		draw();
+
+		function draw(){
+      var mult = size * size;
+  		var squareSize = 500/size;
+
+ 			for(var i = 0; i < mult; i++){
+					$('#wrap').append('<div class="block"></div>');
+  			}
+
+      $('.block').mouseenter(function(){
+
+
+
+        var opac = + $(this).css('opacity')
+        if (opac > 0){
+        	opac -= 0.1;
+          $(this).css({"opacity": opac})
+        }
+
+      });
+
+
+
+			$('.block').css('height', squareSize + 'px');
+			$('.block').css('width', squareSize + 'px');
+		}
+
+    //-------------------------
+
+    $('#button').click(function(){
+       	size = prompt("enter square number");
+
+				$('.block').remove();
+
+        draw();
+
+    });
+
+    //-------------------------
+
+    $('#clear').click(function(){
+    	$('.block').remove();
+    	draw();
+    });
+ 	//------------------------------
+
+
+  for(var g = 0; g < 8; g++){
+
+  	$('#buttons').append('<a href="#" onclick="changeColor(); return false;"><div class="palette"></div></a>');
+    $('#buttons :nth-child('+ (g+3) + ')').css("background-color", color)
   }
+  //------------------------------
 
-  $('.block').mouseenter(function(){
-  	$(this).css("background-color", "black");
-  });
+  	$('#wrap').css('background-color', color);
 
-	$('.block').css('height', squareSize + 'px');
-	$('.block').css('width', squareSize + 'px');
-
+  	function changeColor(){
+    	color="white";
+    }
 
  });
+
+ 
